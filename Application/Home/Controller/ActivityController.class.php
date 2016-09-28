63f4d904d238f144708888284d16b8896015596e
logan <logan@example.com>
2016-09-28 09:09:54 +0800

    modify

--- Application/Home/Controller/ActivityController.class.php
+++ Application/Home/Controller/ActivityController.class.php
@@ -14,7 +14,67 @@ class ActivityController extends BaseController {
 		$this->display();
 	}
 
+    public function del($id)
+    {
+        if ($id) {
+            unlink($_SERVER['DOCUMENT_ROOT'].'/Public/'.M('activity')->where('id='.$id)->getfield('pic'));
+            M('activity')->delete($id);
+        }
 
+        $this->redirect("Activity/index");
+    }
+
+    public function edit()
+    {
+        if (IS_POST) {
+            if ($_FILES['pic']['name']) {
+                $_POST['pic'] = $this->uploadImg('activity_'.I('post.id'));
+            } else {
+                unset($_POST['pic']);
+            }
+
+            $about = M('activity');
+
+            if($about->create()){
+                $about->save(); // 写入数据到数据库
+            }
+
+            $this->redirect('Activity/index');
+        } else {
+            if (!I('get.id')) { 
+                $this->error("参数不正确");
+            }            
+        }
+
+
+        $info = M('activity')->where('id='.I('get.id'))->find();
+
+        $this->assign('info', $info);
+        $this->assign('action_url', U('activity/edit'));
+        $this->assign('app_name', 'activity_index');
+
+        $this->display('add');
+    }
+
+    public function add ()
+    {
+        if (IS_POST) {
+            $_POST['pic'] = $this->uploadImg('activity_'.(M('about_slides')->max('id') + 1));
+            $about = M('activity');
+
+            if($about->create()){
+                $about->add(); // 写入数据到数据库
+            }
+
+            $this->redirect('Activity/index');
+        }
+
+        $this->assign('info', array());
+        $this->assign('action_url', U('Activity/add'));
+        $this->assign('app_name', 'activity_index');
+
+        $this->display();
+    }
 
 
 
@@ -28,40 +88,40 @@ class ActivityController extends BaseController {
 
 	public function slides()
     {
-        $lists = M('about_slides')->order('sort Desc')->select();
+        $lists = M('activity_slides')->order('sort Desc')->select();
 
         $this->assign('lists', $lists);
-        $this->assign('app_name', 'about_slides');
+        $this->assign('app_name', 'activity_slides');
 
         $this->display();
     }
 
-    public function del($id)
+    public function slidesDel($id)
     {
         if ($id) {
-            unlink($_SERVER['DOCUMENT_ROOT'].'/Public/'.M('about_slides')->where('id='.$id)->getfield('pic'));
+            unlink($_SERVER['DOCUMENT_ROOT'].'/Public/'.M('activity_slides')->where('id='.$id)->getfield('pic'));
             M('about_slides')->delete($id);
         }
 
-        $this->redirect("About/slides");
+        $this->redirect("Activity/slides");
     }
 
-    public function edit()
+    public function slidesEdit()
     {
         if (IS_POST) {
             if ($_FILES['pic']['name']) {
-                $_POST['pic'] = $this->uploadImg('about_slides_'.I('post.id'));
+                $_POST['pic'] = $this->uploadImg('activity_slides_'.I('post.id'));
             } else {
                 unset($_POST['pic']);
             }
 
-            $about = M('about_slides');
+            $about = M('activity_slides');
 
             if($about->create()){
                 $about->save(); // 写入数据到数据库
             }
 
-            $this->redirect('About/slides');
+            $this->redirect('Activity/slides');
         } else {
             if (!I('get.id')) { 
                 $this->error("参数不正确");
@@ -69,31 +129,31 @@ class ActivityController extends BaseController {
         }
 
 
-        $info = M('about_slides')->where('id='.I('get.id'))->find();
+        $info = M('activity_slides')->where('id='.I('get.id'))->find();
 
         $this->assign('info', $info);
-        $this->assign('action_url', U('About/edit'));
-        $this->assign('app_name', 'about_slides');
+        $this->assign('action_url', U('Activity/slidesEdit'));
+        $this->assign('app_name', 'activity_slides');
 
-        $this->display('add');
+        $this->display('slidesAdd');
     }
 
-    public function add ()
+    public function slidesAdd ()
     {
         if (IS_POST) {
-            $_POST['pic'] = $this->uploadImg('about_slides_'.(M('about_slides')->max('id') + 1));
-            $about = M('about_slides');
+            $_POST['pic'] = $this->uploadImg('activity_slides_'.(M('activity_slides')->max('id') + 1));
+            $about = M('activity_slides');
 
             if($about->create()){
                 $about->add(); // 写入数据到数据库
             }
 
-            $this->redirect('About/slides');
+            $this->redirect('Activity/slides');
         }
 
         $this->assign('info', array());
-        $this->assign('action_url', U('About/add'));
-        $this->assign('app_name', 'about_slides');
+        $this->assign('action_url', U('Activity/slidesAdd'));
+        $this->assign('app_name', 'activity_slides');
 
         $this->display();
     }
--- Application/Home/Controller/ProjectController.class.php
+++ Application/Home/Controller/ProjectController.class.php
@@ -6,14 +6,75 @@ use Home\Controller\BaseController;
 class ProjectController extends BaseController {
 	public function index () 
 	{
-		$lists = M('activity')->order('create_time Desc')->select();
+		$lists = M('project')->order('create_time Desc')->select();
 
         $this->assign('lists', $lists);
-        $this->assign('app_name', 'activity_index');
+        $this->assign('app_name', 'project_index');
 
 		$this->display();
 	}
 
+    public function del($id)
+    {
+        if ($id) {
+            unlink($_SERVER['DOCUMENT_ROOT'].'/Public/'.M('project')->where('id='.$id)->getfield('pic'));
+            M('project')->delete($id);
+        }
+
+        $this->redirect("Project/index");
+    }
+
+    public function edit()
+    {
+        if (IS_POST) {
+            if ($_FILES['pic']['name']) {
+                $_POST['pic'] = $this->uploadProjectImg('project_'.I('post.id'));
+            } else {
+                unset($_POST['pic']);
+            }
+
+            $about = M('project');
+
+            if($about->create()){
+                $about->save(); // 写入数据到数据库
+            }
+
+            $this->redirect('Project/index');
+        } else {
+            if (!I('get.id')) { 
+                $this->error("参数不正确");
+            }            
+        }
+
+
+        $info = M('project')->where('id='.I('get.id'))->find();
+
+        $this->assign('info', $info);
+        $this->assign('action_url', U('Project/edit'));
+        $this->assign('app_name', 'project_index');
+
+        $this->display('add');
+    }
+
+    public function add ()
+    {
+        if (IS_POST) {
+            $_POST['pic'] = $this->uploadProjectImg('project_'.(M('about_slides')->max('id') + 1));
+            $about = M('project');
+
+            if($about->create()){
+                $about->add(); // 写入数据到数据库
+            }
+
+            $this->redirect('Project/slides');
+        }
+
+        $this->assign('info', array());
+        $this->assign('action_url', U('Project/add'));
+        $this->assign('app_name', 'project_index');
+
+        $this->display();
+    }
 
 
 
@@ -28,40 +89,40 @@ class ProjectController extends BaseController {
 
 	public function slides()
     {
-        $lists = M('about_slides')->order('sort Desc')->select();
+        $lists = M('project_slides')->order('sort Desc')->select();
 
         $this->assign('lists', $lists);
-        $this->assign('app_name', 'about_slides');
+        $this->assign('app_name', 'project_slides');
 
         $this->display();
     }
 
-    public function del($id)
+    public function slidesDel($id)
     {
         if ($id) {
-            unlink($_SERVER['DOCUMENT_ROOT'].'/Public/'.M('about_slides')->where('id='.$id)->getfield('pic'));
+            unlink($_SERVER['DOCUMENT_ROOT'].'/Public/'.M('project_slides')->where('id='.$id)->getfield('pic'));
             M('about_slides')->delete($id);
         }
 
-        $this->redirect("About/slides");
+        $this->redirect("Project/slides");
     }
 
-    public function edit()
+    public function slidesEdit()
     {
         if (IS_POST) {
             if ($_FILES['pic']['name']) {
-                $_POST['pic'] = $this->uploadImg('about_slides_'.I('post.id'));
+                $_POST['pic'] = $this->uploadProjectImg('project_slides_'.I('post.id'));
             } else {
                 unset($_POST['pic']);
             }
 
-            $about = M('about_slides');
+            $about = M('project_slides');
 
             if($about->create()){
                 $about->save(); // 写入数据到数据库
             }
 
-            $this->redirect('About/slides');
+            $this->redirect('Project/slides');
         } else {
             if (!I('get.id')) { 
                 $this->error("参数不正确");
@@ -69,31 +130,31 @@ class ProjectController extends BaseController {
         }
 
 
-        $info = M('about_slides')->where('id='.I('get.id'))->find();
+        $info = M('project_slides')->where('id='.I('get.id'))->find();
 
         $this->assign('info', $info);
-        $this->assign('action_url', U('About/edit'));
-        $this->assign('app_name', 'about_slides');
+        $this->assign('action_url', U('Project/slidesEdit'));
+        $this->assign('app_name', 'project_slides');
 
-        $this->display('add');
+        $this->display('slidesAdd');
     }
 
-    public function add ()
+    public function slidesAdd ()
     {
         if (IS_POST) {
-            $_POST['pic'] = $this->uploadImg('about_slides_'.(M('about_slides')->max('id') + 1));
-            $about = M('about_slides');
+            $_POST['pic'] = $this->uploadProjectImg('project_slides_'.(M('project_slides')->max('id') + 1));
+            $about = M('project_slides');
 
             if($about->create()){
                 $about->add(); // 写入数据到数据库
             }
 
-            $this->redirect('About/slides');
+            $this->redirect('Project/slides');
         }
 
         $this->assign('info', array());
-        $this->assign('action_url', U('About/add'));
-        $this->assign('app_name', 'about_slides');
+        $this->assign('action_url', U('Project/slidesAdd'));
+        $this->assign('app_name', 'project_slides');
 
         $this->display();
     }
