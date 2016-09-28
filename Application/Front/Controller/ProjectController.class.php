9ede9e75c8c083704120fbff66997ff1fae696f5
logan <logan@example.com>
2016-09-28 16:16:26 +0800

    front

--- Application/Front/Controller/ProjectController.class.php
+++ Application/Front/Controller/ProjectController.class.php
@@ -4,22 +4,39 @@ use Think\Controller;
 
 class ProjectController extends Controller {
     public function index(){
-    	$info = M("project")->select();
-        $this->assign("info", $info);
+        $slide = M('Project_slides')->field('id,pic,sort,url')->order('sort desc')->limit(6)->select();
+        $this->assign('slide', $slide);
 
-        $infoSlides = M('project_slides')->select();
-        $this->assign('infoSlides', $infoSlides);
+        if (!I("get.category")) {
+            $full = M('Project')->field('id,pic,type')->order('id desc')->select();
+            $this->assign('full', $full);
+        }
 
+        if ('bs' == I("get.category")) {
+            $house = M('Project')->field('id,pic,type')->where(array('type' => '别墅'))->order('id desc')->select();
+            $this->assign('full',$house);
+            $this->assign('current_category', 'bs');
+        }
+
+        if ('gc' == I('get.category'))
+        {
+            $high = M('Project')->field('id,pic,type')->where(array('type' => '高层'))->order('id desc')->select();
+            $this->assign('full', $high);
+            $this->assign('current_category', 'gc');
+
+        }
+
+        if ('sy' == I("get.category")) {
+            $shop = M('Project')->field('id,pic,type')->where(array('type' => '商业'))->order('id desc')->select();
+            $this->assign('full',$shop);
+            $this->assign('current_category', 'sy');
+        }
         $this->display();
     }
 
-    public function profile()
-    {
-    	$id = I('get.id');
-
-    	$info = M('project')->find($id);
-    	$this->assign('info', $info);
-
-    	$this->display();
+    public function details(){
+        $pro=M('Project a')->field('a.id,a.title,a.desc,a.pic,a.tel,a.url_720')->where('a.id='.I('get.id'))->find();
+        $this->assign('pro',$pro);
+        $this->display();
     }
 }
