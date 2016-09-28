e038b4b85547f008ecec2a3de1818e3ed962577b
logan <logan@example.com>
2016-09-28 17:11:32 +0800

    wxzy  tables

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,11 +8,11 @@ return array(
 
     //数据库配置
     'DB_TYPE'   => 'mysqli',
-    'DB_HOST'   => 'localhost',
+    'DB_HOST'   => 'rdsaz127ifx211kiepm4i.mysql.rds.aliyuncs.com',
     'DB_PORT'   => '3306',
     'DB_NAME'   => 'db_wxzy',
-    'DB_USER'   => 'root',
-    'DB_PWD'    => 'admin',
+    'DB_USER'   => 'db_wxzy',
+    'DB_PWD'    => 'DL_wxzy_tables',
     'DB_PREFIX' => 'wxzy_',
 
 
