97513feda73ac6d39ec3383ec5830f288ad64304
logan <logan@example.com>
2016-09-29 10:49:05 +0800

    magazine modify

--- Application/Front/Controller/MagazineController.class.php
+++ Application/Front/Controller/MagazineController.class.php
@@ -4,28 +4,22 @@ use Think\Controller;
 
 class MagazineController extends Controller {
     public function index(){
-    	$info = M("magazine")->order('create_time DESC')->select();
-    	$this->assign('info', $info);
+        $info = M("magazine")->order('create_time DESC')->where('is_essence !=1')->select();
+        $this->assign('info', $info);
 
-		$infos = M("magazine")->order('create_time DESC')->where('is_essence = 1')->select();
-		//($infos);exit;
-		$this->assign('infos', $infos);
+        $infos = M()->table('wxzy_magazine m,wxzy_magazine_slides s')->field('m.id,m.create_time,m.title,s.pic,m.is_essence')->where('m.is_essence = 1')->order('m.create_time desc')->limit(6)->select();
+        $this->assign('infos',$infos);
 
-    	$infoEssence = M('magazine_slides')->select();
-    	$this->assign('infoEssence', $infoEssence);
-
+        $infoEssence = M()->table('wxzy_magazine m,wxzy_magazine_slides s')->field('m.id,m.title,s.pic,m.is_essence')->where('m.is_essence = 1')->find();
+        $this->assign('infoEssence', $infoEssence);
         $this->display();
     }
 
     public function profile()
     {
-    	$id = I('get.id');
-
-    	$info = M('magazine')->find($id);
-        $info['desc'] = htmlspecialchars_decode($info['desc']);
-        
-    	$this->assign('info', $info);
-
-    	$this->display();
+        $id = I('get.id');
+        $info = M('magazine')->find($id);
+        $this->assign('info', $info);
+        $this->display();
     }
 }
